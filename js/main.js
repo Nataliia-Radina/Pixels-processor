5190af96d3f0c9b5223c6bcb24d94084616c59f8
Nataliia Radina <shyianovska@gmail.com>
2016-05-22 20:36:10 +0200

    add animation, improve mobile-version

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-var controller = new ScrollMagic.Controller();
+ var controller = new ScrollMagic.Controller();
 
+   $(function () { // wait for document ready
+    var processor = TweenMax.to("#processor", 1, {top: "33.43%", ease: Linear.easeNone});
+    var cooler = TweenMax.to("#cooler", 1, {top: "22.38%", ease: Linear.easeNone});
+     var slogan = TweenMax.to("#slogan", 1, {opacity: "1", ease: Linear.easeNone});
+        // build scene
+        new ScrollMagic.Scene({duration: 600})
+                .setPin("#home")
+                .addTo(controller);
+          new ScrollMagic.Scene({duration: 300})
+                .setTween(processor)
+                .addTo(controller);
+        new ScrollMagic.Scene({duration: 300, offset: 300})
+                .setTween(cooler)
+                .addTo(controller);
+         new ScrollMagic.Scene({
+                        duration: 0,
+                        offset: 600
+                    })
+                .setTween(slogan)
+                .addTo(controller);
+      });
